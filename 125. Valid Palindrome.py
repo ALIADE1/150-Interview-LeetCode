fd023e0baec69ae561cbab425bb2286169cfd5a9
Ali Hashhash <155690255+ALIADE1@users.noreply.github.com>
2025-02-15 21:24:08 +0200

    Create 125. Valid Palindrome.py

---
+++ 125. Valid Palindrome.py
@@ -0,0 +1,10 @@
+# Problem: https://leetcode.com/problems/valid-palindrome/description/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        ss = ""
+        for x in s:
+            if x.isalpha():
+                ss+=x.lower()
+        return  ss == ss[::-1]
+      
