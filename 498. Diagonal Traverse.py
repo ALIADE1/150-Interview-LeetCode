e13946d1c5a73816207103c2a6962b5ecaefcbed
Ali Hashhash <155690255+ALIADE1@users.noreply.github.com>
2025-08-25 06:32:27 +0300

    Create 498. Diagonal Traverse.py

---
+++ 498. Diagonal Traverse.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        m, n = len(mat), len(mat[0])
+        result = []
+        row, col, direction = 0, 0, 1
+        
+        while len(result) < m * n:
+            result.append(mat[row][col])
+            
+            if direction == 1:
+                if col == n - 1:
+                    row += 1
+                    direction = -1
+                elif row == 0:
+                    col += 1
+                    direction = -1
+                else:
+                    row -= 1
+                    col += 1
+                    
+            else:
+                if row == m - 1:
+                    col += 1
+                    direction = 1
+                elif col == 0:
+                    row += 1
+                    direction = 1
+                else:
+                    row += 1
+                    col -= 1
+                    
+        return result
