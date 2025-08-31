d52396cdd93107ef258e6aca6fc3d6210767027e
Ali Hashhash <155690255+ALIADE1@users.noreply.github.com>
2025-08-31 13:31:16 +0300

    Create 2807. Insert Greatest Common Divisors in Linked List.py

---
+++ 2807. Insert Greatest Common Divisors in Linked List.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head
+        while cur and cur.next:
+            nex = gcd(cur.val,cur.next.val)
+            new_node = ListNode(nex)
+            new_node.next = cur.next
+            cur.next = new_node
+            cur = new_node.next
+
+        return head
+      
