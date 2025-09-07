9d76e9c682a705950ef31e4a5054679a6cb30717
Ali Hashhash <155690255+ALIADE1@users.noreply.github.com>
2025-09-07 22:47:10 +0300

    Update 1304. Find N Unique Integers Sum up to Zero.py

--- 1304. Find N Unique Integers Sum up to Zero.py
+++ 1304. Find N Unique Integers Sum up to Zero.py
@@ -4,7 +4,7 @@ class Solution:
             return [0]
         elif n % 2 == 0:
             ans = []
-            c = n / 2
+            c = n // 2
             for x in range(1,c+1):
                 ans.append(x)
             for x in range(1,c+1):
