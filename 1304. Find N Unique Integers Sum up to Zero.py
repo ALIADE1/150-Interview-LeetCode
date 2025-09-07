a2cfcef5b5689c5b102ab56ad2b7db7e5ea4e175
Ali Hashhash <155690255+ALIADE1@users.noreply.github.com>
2025-09-07 22:46:15 +0300

    Create 1304. Find N Unique Integers Sum up to Zero.py

---
+++ 1304. Find N Unique Integers Sum up to Zero.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        if n == 1:
+            return [0]
+        elif n % 2 == 0:
+            ans = []
+            c = n / 2
+            for x in range(1,c+1):
+                ans.append(x)
+            for x in range(1,c+1):
+                ans.append(-x)
+            return ans
+        else:
+            ans = [0]
+            c = n // 2
+            for x in range(1,c+1):
+                ans.append(x)
+            for x in range(1,c+1):
+                ans.append(-1*x)
+            return ans
