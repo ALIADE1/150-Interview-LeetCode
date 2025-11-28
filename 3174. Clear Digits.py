2d621d2513407444a9b22da8c713cb4beda5a4c8
Ali HashHash <155690255+ALIADE1@users.noreply.github.com>
2025-11-28 02:48:07 +0200

    Create 3174. Clear Digits.py

---
+++ 3174. Clear Digits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        stack = []
+        for x in s:
+            if x.isdigit():
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(x)
+
+        return "".join(stack)
