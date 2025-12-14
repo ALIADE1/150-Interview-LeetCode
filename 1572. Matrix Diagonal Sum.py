81212465d5a1a5a018f5f1f7363188abdc446acc
Ali HashHash <155690255+ALIADE1@users.noreply.github.com>
2025-12-14 18:59:12 +0200

    Refactor diagonalSum method for clarity and efficiency

--- 1572. Matrix Diagonal Sum.py
+++ 1572. Matrix Diagonal Sum.py
@@ -2,17 +2,10 @@ class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
         ans_summ = 0
         n = len(mat)
-        midd = n// 2 
-        i_f, j_f = 0, 0
-        i_r, j_r = n - 1, n - 1
-
+        midd = n// 2
 
         for i in range(0,n):
-            ans_summ+=mat[i_f][j_f]
-            ans_summ+=mat[i_r][j_r]
-            i_f+=1
-            j_f+=1
-            i_r-=1
-            j_r-=1
+            ans_summ+=mat[i][i]
+            ans_summ+=mat[i][n - i - 1]
 
         return ans_summ - mat[midd][midd] if n % 2 == 1 else ans_summ
