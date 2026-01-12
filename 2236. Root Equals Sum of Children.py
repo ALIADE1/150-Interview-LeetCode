fc9c40116c9d2145f314df811ad97e07a26d922d
Ali HashHash <155690255+ALIADE1@users.noreply.github.com>
2026-01-12 19:23:18 +0200

    Create 2236. Root Equals Sum of Children.py

---
+++ 2236. Root Equals Sum of Children.py
@@ -0,0 +1,12 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def checkTree(self, root: Optional[TreeNode]) -> bool:
+        r = root.val
+        summ = root.right.val + root.left.val
+
+        return r == summ
